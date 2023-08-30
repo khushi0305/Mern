a7905cd1415accf9d00ff26186db066a6b4d6035
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-08-30 10:56:30 +0530

    react

--- Section 2/frontend/src/App.jsx
+++ Section 2/frontend/src/App.jsx
@@ -3,17 +3,23 @@ import './App.css';
 import Home from './components/Home';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div>
       <h1>My React App</h1>
 
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/login" element={ <Login /> } />
+          <Route path="/signup" element={ <Signup /> } />
+          <Route path="*" element={ <NotFound /> } />
+        </Routes>
+      </BrowserRouter>
 
-      <Signup/>
-
-      <Login/>
     </div>
   );
 }
