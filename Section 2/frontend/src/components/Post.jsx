a6498b1667281866dbc14d42f3977a3b62cfc450
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-09-03 19:46:33 +0530

    post

--- Section 2/frontend/src/components/Post.jsx
+++ Section 2/frontend/src/components/Post.jsx
@@ -5,6 +5,8 @@ const Post = () => {
   const [Likes, setLikes] = useState(0);
 
   const [bookMarked, setBookMarked] = useState(false);
+  
+  const [comment, setcomment] = useState(0);
 
   return (
     <div className='container p-5'>
@@ -15,20 +17,21 @@ const Post = () => {
           <i class="fa-solid fa-ellipsis-vertical float-end p-2"></i></div>
         <img src="https://i.pinimg.com/originals/95/26/4f/95264f98af27ca10405cdda05688ccbf.jpg" className="card-img-top myimg" alt="sunset" />
         <p className='container align-items m-0'>
-          <i className={`fa-regular fa-2x fa-heart p-2`} onClick={() => { setLikes(Likes + 1) }}></i>
-          <i className="fa-regular fa-2x fa-comment p-2"></i>
+          <i className="fa-regular fa-2x fa-heart p-2" onClick={() => { setLikes(Likes + 1) }}></i>
+          <i className="fa-regular fa-2x fa-comment p-2" onClick={() => {setcomment(comment+1)}}></i>
           <i className="fa-regular fa-2x fa-paper-plane p-2 "></i>
           <i className={`fa-${bookMarked ? 'solid' : 'regular'} fa-2x fa-bookmark p-2 float-end`} onClick={() => {setBookMarked(!bookMarked)}} ></i>
         </p>
         <div className="card-body pt-2">
-          {Likes} Likes
-          <h6 className="card-title">@_khushi353_</h6>
+          <b>{Likes} Likes</b>
+          <h6 className="card-title py-2">@_khushi353_</h6>
           <p className="card-text">
             When you can't find the sunshine, <i>be the sunshine...!!</i>❤️🌻
           </p>
           <textarea className='area' placeholder='Comment here...' rows={1}></textarea>
           <p className="card-text">
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <p className='my-com'>View all {comment} comments</p>
+            <small className="text-muted upd">Last updated 3 mins ago</small>
           </p>
         </div>
       </div>
