e96f4c2e1e38ba56dbe9b295fd2b8710dc2fed14
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-09-04 23:47:55 +0530

    ...

--- Section 2/frontend/src/components/ProductList.jsx
+++ Section 2/frontend/src/components/ProductList.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import productData from './dummyData'
 
 const ProductList = () => {
+
+    const [productsArray, setproductsArray] = useState(productData);
   return (
-    <div>ProductList</div>
+    <div>
+
+        <div className='container'>
+            <h1>Product Listing</h1>
+            
+            <div className='row'>
+            {productsArray.map( (product) => (
+                <div className='col-md-3'>
+                    <div className='card'>
+                        <img src= {product.image} alt="" />
+                        <div className='card-body'>
+                        <h4>{product.brand}</h4>
+                        <h3>{product.model}</h3>
+                        <h2>₹{product.price}</h2>
+                    </div>
+                    </div>
+                </div>
+            ) )}
+        </div>
+        </div>
+
+    </div>
   )
 }
 
