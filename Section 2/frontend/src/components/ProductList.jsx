310fef496542e92561471b48bb833dc2d79256fe
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-09-05 11:07:13 +0530

    ...

--- Section 2/frontend/src/components/ProductList.jsx
+++ Section 2/frontend/src/components/ProductList.jsx
@@ -4,18 +4,58 @@ import productData from './dummyData'
 const ProductList = () => {
 
     const [productsArray, setproductsArray] = useState(productData);
+
+    const brands = ['Asus' , 'Lenevo' , 'HP' , 'Dell' , 'Apple']
+
+    const searchProduct = (e) => {
+
+        const search = e.target.value;
+        let filteredData = productData.filter( (product) => {return product.model.toLowerCase().includes(search.toLowerCase())});
+
+        setproductsArray(filteredData);
+    };
+
+    const filterBrand = (e) => {
+        const search = e.target.value;
+        let filteredData = productData.filter( (product) => {return product.model.toLowerCase() === search.toLowerCase()});
+
+        setproductsArray(filteredData); 
+    }
   return (
     <div>
 
-        <div className='container'>
-            <h1>Product Listing</h1>
-            
+        <header className='bg-dark'>
+        <div className='container py-5'>
+            <h1 className='text-center mb-5 text-white fw-bold'>Product Listing Page</h1>
+
+            <input type='text' className='form-control w-75 m-auto' onChange={{searchProduct}}/>
+            </div>
+            </header>
+
+            <div className='row mt-4'>
+                <div className='col-3'>
+                    <div className='card'>
+                        <div className='card-body'>
+                            <h3>Filter Options</h3>
+                            <hr/>
+                            <select className='form-control'>
+                                <option value="">Select Brand</option>
+                                {
+                                    brands.map((b) => (<option value= {b}>{b}</option>))
+                                }
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            <div className='col-9'>
+            <div className='container-fluid'>
             <div className='row'>
             {productsArray.map( (product) => (
-                <div className='col-md-3'>
-                    <div className='card'>
+                <div className='col-md-3 mb-4'>
+                    <div className='card' style={{overflow: 'hidden'}}>
                         <img src= {product.image} alt="" className='my-card-img' />
                         <div className='card-body'>
+                            <p className='text-muted'>{product.sponsored ? 'Sponsored' : ''}</p>
                         <h4>{product.brand}</h4>
                         <h3>{product.model}</h3>
                         <h2>₹{product.price}</h2>
@@ -25,7 +65,8 @@ const ProductList = () => {
             ) )}
         </div>
         </div>
-
+        </div>
+        </div>
     </div>
   )
 }
--- Section 2/frontend/src/components/dummyData.js
+++ Section 2/frontend/src/components/dummyData.js
@@ -5,7 +5,8 @@ const productData = [
         model: 'Zenbook',
         price: 70000,
         rating: 4.5,
-        image: 'https://www.reviews-tablet.com/wp-content/uploads/2019/09/Asus-ZenBook-Pro-Duo-UX581-15.6-inch-4K-UHD-NanoEdge-Bezel-Touch-Intel-Core-i7.jpg'
+        image: 'https://www.reviews-tablet.com/wp-content/uploads/2019/09/Asus-ZenBook-Pro-Duo-UX581-15.6-inch-4K-UHD-NanoEdge-Bezel-Touch-Intel-Core-i7.jpg',
+        sponsored: true
     },
     {
         id: 58697,
@@ -13,7 +14,8 @@ const productData = [
         model: 'Pavalion',
         price: 67000,
         rating: 4.5,
-        image: 'https://www.notebookcheck.net/fileadmin/Notebooks/HP/Pavilion_15-ab022ng/4zu3_HP_Pavilion_15_ab002ng.jpg'
+        image: 'https://www.notebookcheck.net/fileadmin/Notebooks/HP/Pavilion_15-ab022ng/4zu3_HP_Pavilion_15_ab002ng.jpg',
+        sponsored: false
     },
     {
         id: 58697,
@@ -21,7 +23,8 @@ const productData = [
         model: 'Zenbook',
         price: 70000,
         rating: 4.5,
-        image: 'https://www.reviews-tablet.com/wp-content/uploads/2019/09/Asus-ZenBook-Pro-Duo-UX581-15.6-inch-4K-UHD-NanoEdge-Bezel-Touch-Intel-Core-i7.jpg'
+        image: 'https://www.reviews-tablet.com/wp-content/uploads/2019/09/Asus-ZenBook-Pro-Duo-UX581-15.6-inch-4K-UHD-NanoEdge-Bezel-Touch-Intel-Core-i7.jpg',
+        sponsored: true
     },
     {
         id: 58697,
@@ -29,7 +32,8 @@ const productData = [
         model: 'Zenbook',
         price: 70000,
         rating: 4.5,
-        image: 'https://www.reviews-tablet.com/wp-content/uploads/2019/09/Asus-ZenBook-Pro-Duo-UX581-15.6-inch-4K-UHD-NanoEdge-Bezel-Touch-Intel-Core-i7.jpg'
+        image: 'https://www.reviews-tablet.com/wp-content/uploads/2019/09/Asus-ZenBook-Pro-Duo-UX581-15.6-inch-4K-UHD-NanoEdge-Bezel-Touch-Intel-Core-i7.jpg',
+        sponsered: false
     },
     {
         id: 58697,
@@ -37,7 +41,8 @@ const productData = [
         model: 'Zenbook',
         price: 70000,
         rating: 4.5,
-        image: 'https://www.reviews-tablet.com/wp-content/uploads/2019/09/Asus-ZenBook-Pro-Duo-UX581-15.6-inch-4K-UHD-NanoEdge-Bezel-Touch-Intel-Core-i7.jpg'
+        image: 'https://www.reviews-tablet.com/wp-content/uploads/2019/09/Asus-ZenBook-Pro-Duo-UX581-15.6-inch-4K-UHD-NanoEdge-Bezel-Touch-Intel-Core-i7.jpg',
+        sponsered:false
     }
 ];
 
