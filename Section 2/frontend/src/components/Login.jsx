de015600b44bea673254706e0eb3dc5c5a459fee
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-09-08 13:41:54 +0530

    login

--- Section 2/frontend/src/components/Login.jsx
+++ Section 2/frontend/src/components/Login.jsx
@@ -1,26 +1,55 @@
+import { useFormik } from 'formik'
 import React from 'react'
 
 const Login = () => {
-  return (
-    <div className='login-container'>
-      <div className="mycard">
-        <p className="brand">Brand Logo</p>
-        <h2 className="text">Member Login</h2>
-        {/* <p className="text">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima ut soluta
-      error assumenda exercitationem saepe, eligendi necessitatibus ipsam!
-      Accusamus, dolores.
-    </p> */}
-        <form>
-          <label htmlFor="email">Email</label>
-          <input className="myinput" type="email" />
-          <label htmlFor="password">Password</label>
-          <input className="myinput" type="password" />
-          <button className="mybtn">Login</button>
-        </form>
-      </div>
 
+  const loginForm = useFormik({
+    initialValues:{
+      email:'',
+      password:''
+    },
+    onSubmit: (values) => {console.log(values);}
+  })
+  return (
+    <div className='py-5 vh-100 bg-body-secondary'>
+    <div className='col-md-4 mx-auto'>
+      <div className='card'>
+        <div className="card-body">
+          <h2 className='my-3'>LogIn Form</h2>
+          
+        <form onSubmit={loginForm.handleSubmit}>
+          
+          <label>Email</label>
+          <input type="text" id='email' onChange={loginForm.handleChange} value={loginForm.values.email} className='form-control mb-3' />
+          
+          <label>Password</label>
+          <input type="password" id='password' onChange={loginForm.handleChange} value={loginForm.values.password} className='form-control mb-3' />
+
+          <button type='submit' className='btn btn-primary w-100 mt-4'>Submit</button>
+        </form>
+        </div>
+      </div>
     </div>
+</div>
+    // <div className='login-container'>
+    //   <div className="mycard">
+    //     <p className="brand">Brand Logo</p>
+    //     <h2 className="text">Member Login</h2>
+    //     {/* <p className="text">
+    //   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima ut soluta
+    //   error assumenda exercitationem saepe, eligendi necessitatibus ipsam!
+    //   Accusamus, dolores.
+    // </p> */}
+    //     <form>
+    //       <label htmlFor="email">Email</label>
+    //       <input className="myinput" type="email" />
+    //       <label htmlFor="password">Password</label>
+    //       <input className="myinput" type="password" />
+    //       <button className="mybtn">Login</button>
+    //     </form>
+    //   </div>
+
+    // </div>
   )
 }
 
