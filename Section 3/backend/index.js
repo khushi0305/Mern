748e170f90b4325722592c45767b47a3cfeccbd9
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-09-10 15:15:57 +0530

    ....

--- Section 3/backend/index.js
+++ Section 3/backend/index.js
@@ -5,6 +5,10 @@ const express = require('express');                                 //backend me
 const app = express();
 const port = 5000;
 
+app.get('/' , (req, res) => {
+    res.send('response from express');
+});
+
 app.listen(port, () => {
-    console.log('server started');
+    console.log('express server started');
 });
