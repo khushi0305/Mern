cffd9f85fb4468cb653051b75ca6779e9f14a5cf
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-09-10 15:30:26 +0530

    ....

--- Section 3/backend/index.js
+++ Section 3/backend/index.js
@@ -9,6 +9,21 @@ app.get('/' , (req, res) => {
     res.send('response from express');
 });
 
+//home
+app.get('/home' , (req, res) => {
+    res.send('response from home');
+});
+
+//add
+app.get('/add' , (req, res) => {
+    res.send('response from add');
+});
+
+//getall
+app.get('/all' , (req, res) => {
+    res.send('response from all');
+});
+
 app.listen(port, () => {
-    console.log('express server started');
+    console.log('server started');
 });
