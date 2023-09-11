54e7b86d22a627cfbe34104e4e95ee26d5bcad4b
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-09-11 12:32:54 +0530

    ....

--- Section 3/backend/connection.js
+++ Section 3/backend/connection.js
@@ -2,4 +2,11 @@ const mongoose = required('mongoose');
 
 const url = "mongodb+srv://khushi353:cXKfX47UexsP6gU@cluster0.bwxp2qu.mongodb.net/mydayabase?retryWrites=true&w=majority";
 
-mongoose.connect(url)
+mongoose.connect(url)                  //promise object
+.then((result) => {                        //thenc snippet
+    console.log('database connected');
+}).catch((err) => {
+    console.log(err);
+});
+
+console.log('Another statement');
