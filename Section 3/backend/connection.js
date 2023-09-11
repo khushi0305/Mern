af94e6037a88b52950e03e9af7c7098a5de41b2a
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-09-11 12:27:33 +0530

    mongoose

---
+++ Section 3/backend/connection.js
@@ -0,0 +1,5 @@
+const mongoose = required('mongoose');
+
+const url = "mongodb+srv://khushi353:cXKfX47UexsP6gU@cluster0.bwxp2qu.mongodb.net/mydayabase?retryWrites=true&w=majority";
+
+mongoose.connect(url)
