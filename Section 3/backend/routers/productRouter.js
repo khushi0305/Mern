8f5aca00b9b4797ff5f0de729e91de0b619f2e95
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-09-12 14:52:08 +0530

    ...

--- Section 3/backend/routers/productRouter.js
+++ Section 3/backend/routers/productRouter.js
@@ -1,20 +1,20 @@
 const express = require('express');
 
-const product = express.Router();
+const router = express.Router();
 
-product.get('/add', (req, res) => {
+router.get('/add', (req, res) => {
     res.send('response from product add')
 })
 
-product.get('/getall', (req, res) => {
+router.get('/getall', (req, res) => {
     res.send('response from product getall')
 })
 
-product.get('/getbyid', (req, res) => {
+router.get('/getbyid', (req, res) => {
     res.send('response from product getbyid')
 })
-product.get('/delete', (req, res) => {
+router.get('/delete', (req, res) => {
     res.send('response from product delete')
 })
 
-module.exports = product;
+module.exports = router;
