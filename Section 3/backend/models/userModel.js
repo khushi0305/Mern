a5c34f7cb64ec9807de257bae7b14716ebfe4bca
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-09-14 12:40:44 +0530

    ..

--- Section 3/backend/models/userModel.js
+++ Section 3/backend/models/userModel.js
@@ -3,7 +3,9 @@ const {Schema, model} = require('../connection');
 const myschema = new Schema({
     name : String,
     email : String,
-    password : String
+    password : String,
+    phNo: String,
+    username: String
 });
 
 module.exports = model('users', myschema);
