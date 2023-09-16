bff394277f89016eca45df4062047c92fb06a7c0
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-09-16 23:34:22 +0530

    ...

--- Section 3/backend/routers/userRouter.js
+++ Section 3/backend/routers/userRouter.js
@@ -11,7 +11,7 @@ router.post('/add', (req, res) => {
     }).catch((err) => {
         console.log(err);
         res.json(err);
-    })
+    });
 
 
     // res.send('respond from user add')
@@ -19,8 +19,27 @@ router.post('/add', (req, res) => {
 
 
 router.get('/getall', (req, res) => {
-    res.send('respond from user getall')
+    Model.find({})
+    .then((result) => {
+        res.json(result);
+    }).catch((err) => {
+        res.json(err);
+    });
+    // res.send('respond from user getall')
 });
+
+//: denotes url parameter
+// router.get('/getbyemail/:email', (req, res) => {
+
+//     console.log(req.params.email);
+
+//     Model.find({email : req.params.email})
+//     .then((result) => {
+//         res.json(result);
+//     }).catch((err) => {
+//         res.json(err);
+//     });
+// })
 
 router.get('/getbyid', (req, res) => {
     res.send('respond from user getbyid')
