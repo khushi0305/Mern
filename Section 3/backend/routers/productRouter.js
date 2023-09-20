99a877ee5a3121ebfeb01a72d98f3ee84333b86a
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-09-20 23:50:10 +0530

    ..

--- Section 3/backend/routers/productRouter.js
+++ Section 3/backend/routers/productRouter.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/add', (req, res) => {
-    res.send('response from product add')
+router.post('/add', (req, res) => {
+    console.log(body.req);
 })
 
 router.get('/getall', (req, res) => {
